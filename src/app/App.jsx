3e1f13de5e9ee59e6ed0b44201f89eb38bdcf0f2
Avcı, Umut <umut.avci@study.thws.de>
2025-06-25 17:47:48 +0200

    .

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "../features/auth/pages/LoginPage";
 import Sidebar from "../components/Sidebar";
-import MoviesPage from "../features/auth/pages/MoviesPage";
+import MoviesPage from "../features/admin/MoviesPage";
+import OverviewPage from "../features/admin/OverviewPage";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Sidebar />} />
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/home" element={<OverviewPage />} />
                 <Route path="/admin" element={<div>Admin Dashboard</div>} />
                 <Route path="/clerk" element={<div>Ticket Clerk Panel</div>} />
             </Routes>
