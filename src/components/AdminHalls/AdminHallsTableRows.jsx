ac96ac7fa8009cd5d378b24b8e9edba3d9a27568
umutVci <umutavci03@gmail.com>
2025-06-27 23:36:31 +0200

    some changes

--- src/components/AdminHalls/AdminHallsTableRows.jsx
+++ src/components/AdminHalls/AdminHallsTableRows.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {PencilAltIcon, XCircleIcon} from "@heroicons/react/solid";
-import TicketClerkService from "../../services/TicketClerkService";
-import UpdateHall from "../../features/admin/AdminHalls/UpdateHall";
-import {Navigate, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import HallService from "../../services/HallService";
 
 const HallsRow = ({ hall }) => {
     const navigate = useNavigate();
     const handleUpdate = () => {
         navigate("/admin/update-hall", {state:{hall}});
+    }
+    const handleDelete = () => {
+        HallService.delete(hall.id)
     }
 
     return (
@@ -17,7 +19,7 @@ const HallsRow = ({ hall }) => {
             <td className="px-4 py-3 text-center">{hall.capacity}</td>
             <td className="px-4 py-3 text-center">{hall.type}</td>
             <td className="px-4 py-3 text-center"><button onClick={handleUpdate}><PencilAltIcon className="w-5 h-5"></PencilAltIcon></button>
-                <button><XCircleIcon className="mr-5 w-5 h-5"></XCircleIcon></button></td>
+                <button onClick={handleDelete}><XCircleIcon className="mr-5 w-5 h-5"></XCircleIcon></button></td>
         </tr>
     )
 }
